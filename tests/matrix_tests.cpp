ff3c660c307ef35590cefc90443d6a12f8e59341
Mojtaba Tabatabaeipour <smt.moji@gmail.com>
2025-09-17 23:00:55 +0200

    Add test for addition

--- tests/matrix_tests.cpp
+++ tests/matrix_tests.cpp
@@ -14,23 +14,6 @@ TEST(MatrixTest, ListInitializer) {
   EXPECT_EQ(m(1, 1), 4);
 }
 
-TEST(MatrixTest, Transpose) {
-  Matrix<double> m = {{1, 2}, {3, 4}};
-  auto res = m.transpose();
-  Matrix<double> ex = {{1, 3}, {2, 4}};
-  for (auto i = 0; i < 2; ++i)
-    for (auto j = 0; j < 2; ++j)
-      EXPECT_EQ(res(i, j), ex(i, j));
-}
-
-TEST(MatrixTest, Inverse) {
-  Matrix<double> m = {{1, 0}, {0, 1}};
-  auto res = m.inverse();
-  for (auto i = 0; i < 2; ++i)
-    for (auto j = 0; j < 2; ++j)
-      EXPECT_EQ(res(i, j), m(i, j));
-}
-
 TEST(MatrixTest, Equality) {
   Matrix<double> a = {{1, 0}, {0, 1}};
   Matrix<double> b = {{1, 0}, {0, 1}};
@@ -40,4 +23,23 @@ TEST(MatrixTest, Equality) {
   EXPECT_EQ(a == c, false);
   Matrix<double> d = {{1}, {2}};
   EXPECT_EQ(a == d, false);
+}
+
+TEST(MatrixTest, Addition) {
+  Matrix<double> a = {{1, 0}, {0, 1}};
+  Matrix<double> b = {{1, 0}, {0, 1}};
+  Matrix<double> res = {{2, 0}, {0, 2}};
+  EXPECT_EQ(a + b, res);
+}
+TEST(MatrixTest, Transpose) {
+  Matrix<double> m = {{1, 2}, {3, 4}};
+  auto res = m.transpose();
+  Matrix<double> ex = {{1, 3}, {2, 4}};
+  EXPECT_EQ(res, ex);
+}
+
+TEST(MatrixTest, Inverse) {
+  Matrix<double> m = {{1, 0}, {0, 1}};
+  auto res = m.inverse();
+  EXPECT_EQ(m, res);
 }
